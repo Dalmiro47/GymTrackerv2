5908864936f864f57d1bba273298976a92f05f9c
Dalmiro47 <dalmiro.dossantos@hotmail.com>
2025-07-07 21:37:16 +0200

    Update LoggedExerciseCard.tsx

--- src/components/training-log/LoggedExerciseCard.tsx
+++ src/components/training-log/LoggedExerciseCard.tsx
@@ -190,7 +190,7 @@ export function LoggedExerciseCard({
       </Card>
 
       <Dialog open={isHistoryDialogOpen} onOpenChange={setIsHistoryDialogOpen}>
-        <DialogContent className="sm:max-w-3xl">
+        <DialogContent className="p-4 sm:p-6 sm:max-w-3xl">
            <DialogHeader>
             <DialogTitle className="font-headline text-xl">
               {loggedExercise.name} - Volume History
