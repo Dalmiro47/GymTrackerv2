756ac78aee24f61e17121265c474cafccb64f8e3
Dalmiro47 <dalmiro.dossantos@hotmail.com>
2025-09-04 16:05:48 +0000

    that's not a bicep icon, it should be like one of the picture attached

--- src/components/icons/BicepIcon.tsx
+++ src/components/icons/BicepIcon.tsx
@@ -18,18 +18,9 @@ export function BicepIcon(props: React.SVGProps<SVGSVGElement>) {
       aria-hidden="true"
       {...props}
     >
-      {/* fist/forearm */}
-      <path d="M13 8c1.2-.6 2.4-1 3.5-1 .8 0 1.5.2 2 .5" />
-      <path d="M18.5 7.5c.4.7.5 1.6.2 2.5-.4 1.2-1.6 2-2.9 2H14" />
-
-      {/* bicep/upper arm curve */}
-      <path d="M14 12c-1.8 0-3.3-.6-4.5-2l-1.2-1.4" />
-
-      {/* shoulder/torso anchor */}
-      <path d="M6.5 7.5C5 8.6 4 10.3 4 12c0 3.3 2.7 6 6 6h5c2.2 0 4-1.8 4-4v-1" />
-
-      {/* elbow/belly of the bicep */}
-      <path d="M9.5 10.5c.8 1 1.8 1.5 3 1.5" />
+      <path d="M8 12.5a5 5 0 1 0 10 0" />
+      <path d="M12.5 7.5V12L10 14" />
+      <path d="M8 12.5h-1a2 2 0 0 0-2 2v4a2 2 0 0 0 2 2h2" />
     </svg>
   );
 }
